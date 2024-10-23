41cf118692686a955c3224c65d41bf74a8d64f58
codewithsaffy <codewithsaffy@gmail.com>
2024-10-24 00:29:29 +0500

    correct con

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,48 @@
-"use client"
-import Link from 'next/link'
-import React, { useRef, useState } from 'react'
-import { FaTimes } from 'react-icons/fa'
-import { IoMenu } from 'react-icons/io5'
+"use client";
+import Link from "next/link";
+import React, { useRef, useState } from "react";
+import { FaTimes } from "react-icons/fa";
+import { IoMenu } from "react-icons/io5";
 
 const Header = () => {
-  const [visible , setVisible] = useState(true)
-  const ref = useRef<HTMLElement>(null)
+  const [visible, setVisible] = useState(false);
+  const ref = useRef<HTMLElement>(null);
   return (
     <header className="header">
-        <Link href="#" className="logo">
-          Portfolio
+      <Link href="#" className="logo">
+        Portfolio
+      </Link>
+      {visible ? (
+        <FaTimes
+          id="menu-icon"
+          className="icon-size"
+          onClick={() => {
+            ref.current!.style.display = "none";
+            setVisible(false);
+          }}
+        />
+      ) : (
+        <IoMenu
+          id="menu-icon"
+          className=" icon-size"
+          onClick={() => {
+            ref.current!.style.display = "flex";
+            setVisible(true);
+          }}
+        />
+      )}
+
+      <nav ref={ref} className="navbar">
+        <Link href="#home" className="active">
+          Home
         </Link>
-{
-  visible ? <FaTimes className='menu-icon icon-size' onClick={()=>{
-    ref.current!.style.display = "none"
-    setVisible(false)
-  }} /> :         <IoMenu className='menu-icon icon-size' onClick={()=>{
-    ref.current!.style.display = "flex"
-    setVisible(true)
-  }}/>
-}
+        <Link href="#about">About</Link>
+        <Link href="#services">Services</Link>
+        <Link href="#projects">Portfolio</Link>
+        <Link href="#contact">Contact</Link>
+      </nav>
+    </header>
+  );
+};
 
-        
-
-        <nav ref={ref} className="navbar">
-          <Link href="#home" className="active">
-            Home
-          </Link>
-          <Link href="#about">About</Link>
-          <Link href="#services">Services</Link>
-          <Link href="#projects">Portfolio</Link>
-          <Link href="#contact">Contact</Link>
-        </nav>
-      </header>
-  )
-}
-
-export default Header
+export default Header;
